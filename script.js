5e20f678354b9429febb11fcc71fc28840a2dd1f
AibelBejoy <aibelbejoy@gmail.com>
2024-09-27 08:00:29 +0530

    bruh

--- script.js
+++ script.js
@@ -1,30 +1,98 @@
-var LenregNo;
-function reg(){
-  if(document.querySelector('.regNo').value.length===6){
+var validation = false;
+function reg() {
+  let regNo = +document.querySelector('.regNo').value;
+  console.log(typeof regNo);
+
+  if (document.querySelector('.regNo').value.length === 6 && !isNaN(regNo)) {
     document.querySelector('.regNo-c p').innerHTML = "Valid!";
+    validation = true;
   }
-  else{
-    document.querySelector('.regNo-c p').innerHTML = "invalid!";
+  else {
+    document.querySelector('.regNo-c p').innerHTML = "Invalid!";
+  }
+
+}
+
+
+function validateDOB() {
+
+
+  let dayValue = document.querySelector('.DOB-d').value;
+  let monthValue = document.querySelector('.DOB-m').value;
+  let yearValue = document.querySelector('.DOB-y').value;
+
+
+  let isValid = true;
+
+  if (dayValue < 1 || dayValue > 31) {
+    isValid = false;
+  }
+
+  if (monthValue < 1 || monthValue > 12) {
+    isValid = false;
+  }
+
+
+  if (yearValue <= 2000 || yearValue >= 2010) {
+    isValid = false;
+  }
+
+
+  if (isValid) {
+    document.querySelector('.DOB-c p').innerHTML = "Valid!";
+
+    if (validation) {
+      validation = true;
+    }else{
+      validation=false;
+    }
+
+  } else {
+    document.querySelector('.DOB-c p').innerHTML = "Invalid!";
   }
 }
 
-function dob(){
-  if(document.querySelector('.DOB').value<0 || document.querySelector('.DOB').value>31){
-        document.querySelector('.DOB-c p').innerHTML = "invalid!";
-        document.querySelector('.DOB').setAttribute('maxlength', '2');
-  }
-  else{
-        document.querySelector('.DOB').setAttribute('maxlength', '10');
-        document.querySelector('.DOB-c p').innerHTML = "Valid!";
 
-        if(document.querySelector('.DOB').value.length==2){
-          document.querySelector('.DOB').value = document.querySelector('.DOB').value + '/';
+document.querySelector('.DOB-d').addEventListener('input', validateDOB);
+document.querySelector('.DOB-m').addEventListener('input', validateDOB);
+document.querySelector('.DOB-y').addEventListener('input', validateDOB);
+
+function validatemailId() {
+  let isValid = true;
+  let mailId = document.querySelector('.mailId input').value;
+  if (/[a-z0-9._%+\-]+@[a-z0-9.\-]+\.[a-z]{2,}$/.test(mailId)) {
+    isValid = true;
+  }
+  else {
+    isValid = false;
+  }
+  if (isValid) {
+    document.querySelector('.mailId p').innerHTML = "Valid!";
+    if (validation) {
+      validation = true;
+    }else{
+      validation=false;
     }
   }
-
-  if(document.querySelector('.DOB').value.length==5){
-    document.querySelector('.DOB').value = document.querySelector('.DOB').value + '/';
+  else {
+    document.querySelector('.mailId p').innerHTML = "Invalid!";
   }
-
-
 }
+
+document.querySelector('.mailId input').addEventListener('input', validatemailId);
+
+
+function submit(){
+  if(validation){
+    document.querySelector('.validation').innerHTML="VALID!!";
+  }
+  else{
+    document.querySelector('.validation').innerHTML="INVALID!!";
+  }
+}
+document.querySelector('.formsubmit').addEventListener('click', submit);
+
+
+
+
+
