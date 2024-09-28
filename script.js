6226c620cb81db5edab674185d7b56e987889237
AibelBejoy <aibelbejoy@gmail.com>
2024-09-28 23:27:14 +0530

    Update script.js

--- script.js
+++ script.js
@@ -180,7 +180,7 @@ function emailSend(event) {
 
     sendMail(params);
     console.log("Email sent successfully");
-    setTimeout(() => window.location.href = 'apply-form-received.html', 3000);
+    setTimeout(() => window.location.href = 'apply-form-received.html', 5000);
 
   } else {
     document.querySelector('.validationText').innerHTML = "Form is invalid!";
