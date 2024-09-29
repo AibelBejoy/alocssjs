59eddcb1bfdaab33fd675a9dc7cde61c8cc024e6
AibelBejoy <aibelbejoy@gmail.com>
2024-09-29 19:24:57 +0530

    faculty to departments

--- script.js
+++ script.js
@@ -142,8 +142,11 @@ function validityCheck() {
 
 document.querySelector('.formvalid').addEventListener('click', validityCheck);
 
-
-
+function changeTeacher(teacherImg, teacherName){
+  const imgElement = document.querySelector('.teacher-img')
+  imgElement.src = teacherImg;
+  document.querySelector('.teacher-info').innerHTML = teacherName;
+}
 
 
 //------------------------------------------------IGNORE---------------------------------------------------//
